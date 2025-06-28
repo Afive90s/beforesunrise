279761e05e60c87efb1e01faba36a6fcb3350143
Afive90s <119968000+Afive90s@users.noreply.github.com>
2025-06-28 14:13:29 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+import asyncio
+import logging
+
+from decouple import config
+from telethon import TelegramClient, events
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s ',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+api_id = config('API_ID')
+
+api_hash = config('API_HASH')
+
+client = TelegramClient(
+    'mahdiashtian',
+    api_id,
+    api_hash,
+)
+
+client.start()
+
+
+@client.on(events.NewMessage(func=lambda e: e.is_private and e.media and e.media.ttl_seconds))
+async def downloader(event):
+    result = await event.download_media()
+    await client.send_file("me", result, caption="Downloaded by afivedejongh")
+
+
+asyncio.get_event_loop().run_forever()
+client.run_until_disconnected()
